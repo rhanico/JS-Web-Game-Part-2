5321f10a53f626a31ccb823ffb294ef88c63ef7f
FRANZ RHANICO <109740904+rhanico@users.noreply.github.com>
2023-03-12 01:33:00 -0800

    added the return function

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ function newItem(url, left, bottom){
 let inventory = document.createElement("div")
 
 function newInventory() {
+
+    let inventory = document.createElement("div")
+
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
     inventory.style.left = '0px'
@@ -34,6 +37,7 @@ function newInventory() {
     inventory.style.border = '2px solid black'
     inventory.style.backgroundColor = 'brown'
     document.body.append(inventory)
+    return inventory
 }
 
 newInventory()
